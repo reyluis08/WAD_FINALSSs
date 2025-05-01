0f4aa1e683b51a8b022fa2a8f62dd21b97a4b41c
reyluis08 <reyluispinto8@gmail.com>
2025-05-01 16:02:16 +0800

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,14 +1,7 @@
-from django.urls import path
-from django.views.generic import RedirectView
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='welcome.html', permanent=False)),
-    path('home.html', views.home, name='home'),                # home.html
-    path('about_us.html', views.about_us, name='about'),     # about_us.html
-    path('menu.html', views.menu, name='menu'),           # menu.html
-    path('cart.html', views.cart, name='cart'),           # cart.html
-    path('music.html', views.music, name='music'),        # music.html
-    path('needhelp.html', views.needhelp, name='needhelp'), # needhelp.html
-    path('welcome.html', views.welcome, name='welcome'),  # welcome.html
+    path('admin/', admin.site.urls),
+    path('', include('core.urls')),  # This will route the main pages
 ]
